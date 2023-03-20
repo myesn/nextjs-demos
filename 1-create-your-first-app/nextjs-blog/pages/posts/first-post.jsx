f10a699a28b4f957b798fa1f8c273421558d7c4b
myesn <myesn@foxmail.com>
2023-03-20 17:28:37 +0800

    feat: metadata - adding Head

--- 1-create-your-first-app/nextjs-blog/pages/posts/first-post.jsx
+++ 1-create-your-first-app/nextjs-blog/pages/posts/first-post.jsx
@@ -1,18 +1,24 @@
-import Link from "next/link";
-import Image from "next/image";
+import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
 
 export default function FirstPost() {
+  const title = 'First Post';
+
   return (
     <>
-      <h1>First Post</h1>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <h1>{title}</h1>
       <Image
-        src="/images/profile.jpg" // Route of the image file
+        src='/images/profile.jpg' // Route of the image file
         height={144} // Desired size with correct aspect ratio
         width={144} // Desired size with correct aspect ratio
-        alt="Your Name"
+        alt='Your Name'
       />
       <h2>
-        <Link href="/">Back to home</Link>
+        <Link href='/'>Back to home</Link>
       </h2>
     </>
   );
