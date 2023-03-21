4cdde38ebba91e99a8200536cf7294fb8f13032b
myesn <myesn@foxmail.com>
2023-03-21 17:24:33 +0800

    refactor: layout component and home page

--- 1-create-your-first-app/nextjs-blog/components/layout.jsx
+++ 1-create-your-first-app/nextjs-blog/components/layout.jsx
@@ -1,5 +1,73 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
 import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
 
-export default function Layout({ children }) {
-  return <div className={styles.container}>{children}</div>;
+const name = 'myEsn2E9';
+export const siteTitle = 'Next.js Sample Website';
+
+/**
+ * meta 标签（如 og:image ），用于描述页面内容
+ * Boolean home 属性将调整标题和图像的大小
+ */
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel='icon' href='/favicon.ico' />
+        <meta
+          name='description'
+          content='Learn how to build a personal website using Next.js'
+        />
+        <meta
+          property='og:image'
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name='og:title' content={siteTitle} />
+        <meta name='twitter:card' content='summary_large_image' />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src='/images/profile.jpg'
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt=''
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href='/'>
+              <Image
+                priority
+                src='/images/profile.jpg'
+                className={utilStyles.borderCircle}
+                height={108}
+                width={108}
+                alt=''
+              />
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href='/' className={utilStyles.colorInherit}>
+                {name}
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href='/'>← Back to home</Link>
+        </div>
+      )}
+    </div>
+  );
 }
