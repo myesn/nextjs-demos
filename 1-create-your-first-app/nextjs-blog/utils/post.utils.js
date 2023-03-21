bbec41d714d7c824b0c0c0ae8b7f0ab9c3771769
myesn <myesn@foxmail.com>
2023-03-21 23:10:46 +0800

    feat: creating the utility function to read the file system(parse metadata from markdown files)

---
+++ 1-create-your-first-app/nextjs-blog/utils/post.utils.js
@@ -0,0 +1,41 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+/**
+ * posts 工具函数（Utility function）的作用：
+ *  - 解析每个 markdown 文件并获取 title 、 date 和文件名（将用作帖子 URL 的 id ）。
+ *  - 列出索引页上的数据，按日期排序。
+ */
+
+const postsDirectory = path.join(process.cwd(), 'posts');
+
+export function getSortedPostsData() {
+  // Get file names under /posts
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    // Remove ".md" from file name to get id
+    const id = fileName.replace(/\.md$/, '');
+
+    // Read markdown file as string
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+
+    // Combine the data with the id
+    return {
+      id,
+      ...matterResult.data,
+    };
+  });
+  // Sort posts by date
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
